4ab3965c42b2a6f79fee21f4efab4efb1be53394
Oman M Ijaz <81192723+omanijaz1@users.noreply.github.com>
2021-04-29 18:59:29 -0400

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student
+{
+  public static void main (String [] args);
+  {
+   int studentAge =15;
+   double studentGPA =3.6;
+    char studentFInitial = 'K';
+      char studentLInitial = 'M';
+        boolean hasPerfectAttendence = true;
+         
+         System.out.println(studentAge);
+         System.out.println(studentGPA);
+         System.out.println(studentFInitial);
+         System.out.println(studentLInitial);
+         System.out.println(hasPerfectAttendence);
+  } 
+}
